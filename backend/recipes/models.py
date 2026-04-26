614adfeb17aba3e34818b148743fab33a49b77d9
rita nassar <ritanassar2004@gmail.com>
2026-04-26 23:31:03 +0300

    create models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/images/')
+    pdf_file = models.FileField(upload_to='recipes/pdfs/')
+    created_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=50)
