82d4ad3dc139c7d70fe4c4272236af21f4e21074
Maya <maya.bouban.mb@gmail.com>
2026-04-27 01:54:15 +0300

    implemented views for Items

--- backend/items/views.py
+++ backend/items/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemListView(generics.ListAPIView):
+  queryset=Item.objects.all()
+  serializer_class= ItemSerializer
+
+class ItemCreateView(generics.CreatAPIView):
+  queryset=Item.objects.all()
+  serializer_class= ItemSerializer
+
