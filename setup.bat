8e6a4139ad4e94e9cb6358f2450c67cdd980b344
rita nassar <ritanassar2004@gmail.com>
2026-04-27 01:35:12 +0300

    fix setup.bat to use correct activation and pip commands for virtual environment

--- setup.bat
+++ setup.bat
@@ -1,4 +1,3 @@
-python -m venv .venv
-.venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+py -m venv .venv
+.venv\Scripts\python.exe -m pip install --upgrade pip
+.venv\Scripts\python.exe -m pip install -r requirements.txt
