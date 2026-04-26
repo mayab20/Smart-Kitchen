43b8ffbaec2e07c3a8de77238997dbb4fb94c2bb
rita nassar <ritanassar2004@gmail.com>
2026-04-27 01:25:12 +0300

    create recipe service

---
+++ frontend/src/app/recipe.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Recipe {
+  id?: number;
+  title: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  private apiUrl = 'http://127.0.0.1:8000/api/recipes/';
+
+  constructor(private http: HttpClient) {}
+
+  getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(this.apiUrl);
+  }
+
+  addRecipe(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  deleteRecipe(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}${id}/`);
+  }
+}
