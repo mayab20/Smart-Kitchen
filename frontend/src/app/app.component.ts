68d576981c48289685264641d99d8e809d877315
rita nassar <ritanassar2004@gmail.com>
2026-04-27 01:29:22 +0300

    update AppComponent to include RecipesComponent and set as standalone

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipesComponent } from './recipe/recipe.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
