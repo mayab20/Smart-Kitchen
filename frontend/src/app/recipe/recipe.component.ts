19247b59cdd62f34a62324025f9ff29457041b7c
rita nassar <ritanassar2004@gmail.com>
2026-04-27 01:24:42 +0300

    create recipe component

---
+++ frontend/src/app/recipe/recipe.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-recipes',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './recipe.component.html',
+  styleUrl: './recipe.component.scss'
+})
+export class RecipesComponent implements OnInit {
+
+  recipes: any[] = [];
+
+  newRecipe: any = {
+    title: '',
+    description: '',
+    instructions: ''
+  };
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.loadRecipes();
+  }
+
+  onFileChange(event: any, field: string) {
+  const file = event.target.files[0];
+  this.newRecipe[field] = file;
+}
+
+  loadRecipes() {
+    this.recipeService.getRecipes().subscribe(data => {
+      this.recipes = data;
+    });
+  }
+
+  addRecipe() {
+  const formData = new FormData();
+
+  for (let key in this.newRecipe) {
+    formData.append(key, this.newRecipe[key]);
+  }
+
+  this.recipeService.addRecipe(formData).subscribe(() => {
+    this.loadRecipes();
+  });
+}
+
+  deleteRecipe(id: number) {
+    this.recipeService.deleteRecipe(id).subscribe(() => {
+      this.loadRecipes();
+    });
+  }
+}
