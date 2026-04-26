162a5ef1ec32c2eca7fc483feb2b4d1d00674e45
rita nassar <ritanassar2004@gmail.com>
2026-04-27 01:45:07 +0300

    fix recipe model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,16 +4,29 @@ from django.contrib.auth.models import User
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     instructions = models.TextField()
-    image = models.ImageField(upload_to='recipes/images/', blank=True, null=True)
-    pdf_file = models.FileField(upload_to='recipes/pdfs/', blank=True, null=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to='recipes/images/')
+    pdf_file = models.FileField(upload_to='recipes/pdfs/')
     created_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient'
+    )
+
+    def __str__(self):
+        return self.title
+
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50, blank=True)
