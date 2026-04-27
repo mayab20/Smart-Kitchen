0f6535ae1e1a13a58fe0af079a62896674d6bd45
Maya <maya.bouban.mb@gmail.com>
2026-04-27 16:14:34 +0300

    improved the CRUD on user table

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,37 +1,60 @@
+from rest_framework import viewsets
 from rest_framework import generics
-from .serializers import UserSerializer, EmailUpdateSerializer,PasswordUpdateSerializer
-from .models import User
 from rest_framework.permissions import IsAuthenticated
+from .models import Profile
+from .serializers import ProfileSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import EmailUpdateSerializer
+from .serializers import PasswordUpdateSerializer
 
-class UserProfileView(generics.RetrieveAPIView):
-  serializer_class= UserSerializer
-  permission_classes =[IsAuthenticated]
 
-  def get_object(self):
-    return self.request.user
+class ProfileViewSet(viewsets.ModelViewSet):
+  serializer_class = ProfileSerializer
+  permission_classes = [IsAuthenticated]
 
-class UserCreateView(generics.CreateAPIView):
-  queryset= User.objects.all()
-  serializer_class= UserSerializer
-  permission_classes= [IsAuthenticated]
+  def get_queryset(self):
+    # user can only see their own profile
+    return Profile.objects.filter(user=self.request.user)
 
-class ProfileUpdateView(generics.UpdateAPIView):
-  serializer_class=UserSerializer
-  permission_classes= [IsAuthenticated]
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
 
-  def get_object(self):
-    return self.request.user
-  
-class EmailUpdateView(generics.UpdateAPIView):
-  serializer_class=EmailUpdateSerializer
-  permission_classes= [IsAuthenticated]
+class EmailUpdateView(APIView):
+  permission_classes = [IsAuthenticated]
 
-  def get_object(self):
-    return self.request.user
-  
-class PasswordUpdateView(generics.UpdateAPIView):
-  serializer_class=PasswordUpdateSerializer
-  permission_classes= [IsAuthenticated]
+  def put(self, request):
+    serializer = EmailUpdateSerializer(
+      instance=request.user,
+      data=request.data
+    )
 
-  def get_object(self):
-    return self.request.user
+    if serializer.is_valid():
+      serializer.save()
+      return Response(
+          {"message": "Email updated successfully"},
+          status=status.HTTP_200_OK
+      )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordUpdateView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def put(self, request):
+    serializer = PasswordUpdateSerializer(
+      data=request.data,
+      context={"request": request}
+    )
+
+    if serializer.is_valid():
+      serializer.save()
+      return Response(
+        {"message": "Password updated successfully"},
+        status=status.HTTP_200_OK
+      )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
