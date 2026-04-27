288367b836bad39eba6d5081966324366b9e2c6b
Maya <maya.bouban.mb@gmail.com>
2026-04-27 16:24:36 +0300

    changed the Item view

--- backend/items/views.py
+++ backend/items/views.py
@@ -1,12 +1,8 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Item
 from .serializers import ItemSerializer
 
-class ItemListView(generics.ListAPIView):
-  queryset=Item.objects.all()
-  serializer_class= ItemSerializer
 
-class ItemCreateView(generics.CreatAPIView):
-  queryset=Item.objects.all()
-  serializer_class= ItemSerializer
-
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
