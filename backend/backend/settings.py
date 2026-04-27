0fc5602bc5a66d153efbf0238ef36f0d9a3d1ab7
Maya <maya.bouban.mb@gmail.com>
2026-04-27 23:49:15 +0300

    securing the secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z053lpbu38+@ly57l&ns#&y897^t_y219ffp_$09_t)z7$af8_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
