b79390174dfbcc4df893cab4adaee3677bbebf74
Maya <maya.bouban.mb@gmail.com>
2026-04-28 14:26:14 +0300

    Fix rootDir in tsconfig and clean up imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 
 @Component({
