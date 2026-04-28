1ce9686e1a237a461f060cc5dfddad7ef08e1540
rita nassar <ritanassar2004@gmail.com>
2026-04-28 14:44:54 +0300

    fix: resolve merge conflicts in app.component.html and app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet , RouterLink} from '@angular/router';
-=======
 import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { AuthService } from './auth/auth.service';
->>>>>>> b79390174dfbcc4df893cab4adaee3677bbebf74
 
 @Component({
   selector: 'app-root',
@@ -15,8 +11,6 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent {
   title = 'frontend';
-<<<<<<< HEAD
-=======
 
   constructor(public authService: AuthService, private router: Router) {}
 
@@ -26,5 +20,4 @@ export class AppComponent {
       error: () => this.router.navigate(['/login'])
     });
   }
->>>>>>> b79390174dfbcc4df893cab4adaee3677bbebf74
 }
