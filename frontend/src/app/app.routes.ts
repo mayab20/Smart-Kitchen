4b83489c9a2c8f8bc020d67b7de3c5255a8e181f
Maya <maya.bouban.mb@gmail.com>
2026-04-29 02:59:41 +0300

    added profile and pantry paths to app routes and their links to the navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipe/recipe.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { PantryComponent } from './pantry/pantry.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes = [
   { path: '', component: RecipesComponent },
@@ -11,4 +13,6 @@ export const routes = [
   { path: 'recipe/:id', component: RecipeDetailComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'pantry', component: PantryComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
