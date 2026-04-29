3512070fa2b056b188bec59d741cc7d9e89be4b7
Maya <maya.bouban.mb@gmail.com>
2026-04-29 03:04:56 +0300

    Add user association on recipe creation and implement my_recipes endpoint for the users recipe in the his own profile

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,20 +1,21 @@
 from rest_framework import viewsets
-from .models import Recipe
-from .serializers import RecipeSerializer
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Recipe
+from .serializers import RecipeSerializer
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        
-        if not serializer.is_valid():
-            print(serializer.errors)  # 👈 THIS IS IMPORTANT
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        self.perform_create(serializer)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='my-recipes')
+    def my_recipes(self, request):
+        recipes = Recipe.objects.filter(created_by=request.user)
+        serializer = self.get_serializer(recipes, many=True)
         return Response(serializer.data)
