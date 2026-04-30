8cd678dbc8c97081776b87f0af9ed7fac3a21cfc
rita nassar <ritanassar2004@gmail.com>
2026-04-30 09:36:39 +0300

    Improve authentication handling: persist access token in localStorage and update login/logout methods

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -8,7 +8,13 @@ export class AuthService {
   private apiUrl = 'http://localhost:8000/api';
   private accessToken: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Load token from localStorage on service initialization
+    const storedToken = localStorage.getItem('accessToken');
+    if (storedToken) {
+      this.accessToken = storedToken;
+    }
+  }
 
   signup(username: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/users/register/`, { username, password });
@@ -18,6 +24,7 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/users/login/`, { username, password }, { withCredentials: true }).pipe(
       tap((response: any) => {
         this.accessToken = response.access;
+        localStorage.setItem('accessToken', response.access);
       })
     );
   }
@@ -26,12 +33,21 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/users/logout/`, {}, { withCredentials: true }).pipe(
       tap(() => {
         this.accessToken = null;
+        localStorage.removeItem('accessToken');
       })
     );
   }
 
   isLoggedIn(): boolean {
-    return !!this.accessToken;
+    if (this.accessToken) {
+      return true;
+    }
+    const storedToken = localStorage.getItem('accessToken');
+    if (storedToken) {
+      this.accessToken = storedToken;
+      return true;
+    }
+    return false;
   }
 
   getAccessToken(): string | null {
