35820fd42ca8effc17ed224bb514a33d2f0f07bf
Maya <maya.bouban.mb@gmail.com>
2026-05-01 01:19:15 +0300

    connected the db to the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+      'ENGINE':'django.db.backends.mysql',
+      'NAME':'django_db',
+      'USER':'django_user',
+      'PASSWORD':'1234',
+      'HOST':'127.0.0.1',
+      'PORT':'3306',
+    }
 }
 
 
